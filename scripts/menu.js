9e14627849b424865ede86dc50cefc5cb9500476
VictorManuelTamayo <victortamayo509@gmail.com>
2021-01-19 08:32:38 -0600

    helen

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,28 +1,6 @@
-$(document).ready(function(){
-    var bntMenu = document.querySelector('.btn-menu');
-    var menu = document.querySelector('.menu');
+let btnMenu = document.querySelector('.btnMenu');
+let menu = document.querySelector('.menu');
 
-    $(bntMenu).hide();
-
-    $(window).resize(function(){
-
-        var windowWidth = $(window).width();
-        if(windowWidth > 566){
-            $(bntMenu).hide();
-            $(menu).show();
-        }
-
-        if(windowWidth < 566){
-            $(bntMenu).show();
-            $(menu).hide();
-
-            bntMenu.addEventListener('click',()=>{
-                $(menu).toggle();
-            });
-
-        }
-
-    });
-
-    
-});
+btnMenu.addEventListener('click', ()=>{
+    $(menu).toggleClass('visible');
+})
